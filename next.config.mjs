41b35544b08ec3654a3e18841653ce412bd51db8
PlayTi <tiago.rafael019@gmail.com>
2024-05-13 11:52:01 -0300

    done

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  server: {
-    protocol: "http",
-  },
   async rewrites() {
     return [
       {
         source: "/",
         destination: "/davsummaryreport",
+      },
+      {
+        source: "/api/:path*", // Configura a rota do proxy para o backend HTTP
+        destination: "http://200.233.186.22:3000/api/:path*", // Especifica o destino do backend
       },
     ];
   },
