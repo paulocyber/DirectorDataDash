f47260ffe458a4f8795e5daeae85b2bd07a9d91e
unknown <tiago.rafael019@gmail.com>
2024-12-09 15:31:38 -0300

    Adicionando função outros

--- src/components/layouts/salesByBrand/index.tsx
+++ src/components/layouts/salesByBrand/index.tsx
@@ -27,12 +27,15 @@ import getDate from "@/utils/date/currentDate"
 // Tipagem
 import { SalesByBrandType } from "@/types/salesByBrand"
 import { StockByBrand } from "@/types/stock"
+import { DateRangePicker, DateValue, RangeValue } from "@nextui-org/react"
+import { parseDate } from '@internationalized/date';
 
 enum DateRange {
     Day = 'day',
     Week = 'week',
     Month = 'month',
     MonthYesterday = 'month yesterday',
+    others = 'others',
     Year = 'year'
 }
 
@@ -51,6 +54,7 @@ export default function UiSalesByBrand({ listSalesByBrand, listStockByBrand }: S
     const [stockByBrand, setStockByBrand] = useState(listStockByBrand || [])
     const [selectedDateRange, setSelectedDateRange] = useState<string>('day');
     const [brands, setBrands] = useRecoilState(brandsAtom)
+    const [showDatePicker, setShowDatePicker] = useState<boolean>(false)
     const [loading, setLoading] = useState<boolean>(false)
 
     const { today, year, month, startOfWeek, endOfWeek } = getDate()
@@ -75,36 +79,75 @@ export default function UiSalesByBrand({ listSalesByBrand, listStockByBrand }: S
         switch (date) {
             case DateRange.Day:
                 setDate({ start: today, end: today })
+                setShowDatePicker(false)
                 await fetchSalesByBrand({ token, dateInit: today, dateEnd: today, setLoading, setSalesByBrand, setStockByBrand, brands })
                 break;
 
             case DateRange.Week:
                 setDate({ start: startOfWeek, end: endOfWeek })
-
+                setShowDatePicker(false)
                 await fetchSalesByBrand({ token, dateInit: startOfWeek, dateEnd: endOfWeek, setLoading, setSalesByBrand, setStockByBrand, brands })
                 break;
 
             case DateRange.Month:
                 setDate({ start: `${year}/${month}/01`, end: today })
+                setShowDatePicker(false)
                 await fetchSalesByBrand({ token, dateInit: `${year}/${month}/01`, dateEnd: today, setLoading, setSalesByBrand, setStockByBrand, brands })
                 break;
 
             case DateRange.MonthYesterday:
                 setDate({ start: `${year}/${month - 1}/01`, end: today });
+                setShowDatePicker(false)
                 await fetchSalesByBrand({ token, dateInit: `${year}/${month - 1}/01`, dateEnd: `${year}/${month - 1}/30`, setLoading, setSalesByBrand, setStockByBrand, brands })
+                break;
+
+            case DateRange.others:
+                setShowDatePicker(true)
                 break;
 
             case DateRange.Year:
                 setDate({ start: `${year}/01/01`, end: today })
+                setShowDatePicker(false)
                 await fetchSalesByBrand({ token, dateInit: `${year}/01/01`, dateEnd: today, setLoading, setSalesByBrand, setStockByBrand, brands })
                 break;
         }
     }
 
+    const handleDateRangeChange = async (selected: RangeValue<DateValue> | null) => {
+        if (selected?.start && selected?.end) {
+            const start = selected.start.toString(); // Converte para string
+            const end = selected.end.toString(); // Converte para string
+
+            setDate({ start, end });
+            await fetchSalesByBrand({
+                token,
+                dateInit: start,
+                dateEnd: end,
+                setLoading,
+                setSalesByBrand,
+                setStockByBrand,
+                brands
+            });
+        }
+    };
+
     return (
         <>
             <Container>
                 <ToolBar title="Vendas por marcas" handleRefreshClick={handleRefresh} handleCleanFilter={handleCleanFilter} handleDate={handleData} selectedDateRange={selectedDateRange} displayFormOfPayment={true} />
+                <div className="px-6">
+                    <DateRangePicker
+                        className="max-w-xs"
+                        classNames={{
+                            inputWrapper: `lg:flex bg-blue-700 hover:bg-blue-700 text-white focus-within:hover:bg-white-500`,
+                            base: `text-white  ${!showDatePicker && 'hidden'}`,
+                            innerWrapper: "py-[0.2em] text-white ",
+                            segment: "text-white",
+                            selectorIcon: "text-center text-white",
+                        }}
+                        onChange={handleDateRangeChange}
+                    />
+                </div>
                 <GraphicContainer loading={loading} children={<BarChart data={salesByBrand} dataKey="value" dataKeyXAxis="brand" displayXAxis={true} displayCartesianGrid={true} palette={highLightColor} />} />
             </Container>
             <Container>
