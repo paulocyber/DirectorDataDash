d0bd9ef8c17ef450b8addfa98cc253bb2c74e0b4
PauloCyber <paulocyber@gmail.com>
2025-12-19 12:00:11 -0300

    Revert "done"
    This reverts commit 11e02737b536fa098d98e76935978692cbeeeb54.

--- utils/fetchs/api/index.ts
+++ utils/fetchs/api/index.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export function setupApiClient(ctx?: string) {
   const api = axios.create({
-    baseURL: "https://api.playcell.autotasker.com.br/",
+    baseURL: "https://sistema-suporte-play-uljpe.ondigitalocean.app",
   });
 
   api.interceptors.request.use(
